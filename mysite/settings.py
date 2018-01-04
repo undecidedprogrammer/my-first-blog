bc6a21137a5f861f8338579b19aff33d6a0599ee
Mika <mikagapunay@gmail.com>
2018-01-04 15:46:09 +0800

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,10 +121,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ALLOWED_HOSTS = ['127.0.0.1', 'simpleblogapp.pythonanywhere.com']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
